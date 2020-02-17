23985b3b9eccfa884c8e5be20c8b524325be4597
Justin Singh <justin.singh125125@gmail.com>
2020-02-17 07:57:48 -0800

    deploy test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+var cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +11,7 @@ const app = express();
 //for parsing json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 //if in production, serve up our minified react application
 //aksdjf
